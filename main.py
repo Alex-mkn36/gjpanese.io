4a35315abe0e8936e30794e2632eb24b6adf1a71
Alex-mkn36 <alex.m.k.n36@gmail.com>
2025-07-05 13:34:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
-app = Flask(__name__)
 
+web = Flask(__name__)
 
-@app.route("/")
+@web.route("/")
 def Home():
     return render_template("index.html")
