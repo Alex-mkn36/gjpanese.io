b6394811c526b9b8e6d742e4e87874b38c646ebc
Alex-mkn36 <alex.m.k.n36@gmail.com>
2025-08-26 13:05:48 +1000

    YATAAAAAA

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
+import regex
 import json
 import requests
 import urllib.request
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     else:
         None
 
-
-    x = result['word']
+    grammar = 'から for because'
+    word = result['word']
 
     # instantiate a Chrome browser
     driver = uc.Chrome(
@@ -33,12 +33,12 @@ if __name__ == "__main__":
 
 
     # Wait for the page to load
-    time.sleep(16)
+    time.sleep(4)
 
 
     # Accept cookies if the prompt appears (optional, depends on region)
     try:
-        accept_button = driver.find_element(By.CLASS_NAME, "flex w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-2 h-8 max-h-48 min-h-8 border-none text-base")
+        accept_button = driver.find_element(By.CLASS_NAME, "cm__btn")
         accept_button.click()
         time.sleep(1)
     except:
@@ -47,24 +47,23 @@ if __name__ == "__main__":
 
     # Find the search box and enter "Copilot"
     time.sleep(2)
-    search_box = driver.find_element(By.ID, "userInput")
-    search_box.send_keys(f"Construct a japanese sentance using the word:{x}")
+    search_box = driver.find_element(By.CSS_SELECTOR, 'textarea[aria-label="Talk to me!"]')
+    search_box.send_keys(f"Construct one example Japanese sentence with no explanation using the word: {word}, with the Grammar point {grammar}, sentence complexity of N{level}. Format: Japanese sentence + example")
     search_box.send_keys(Keys.RETURN)  # Press Enter
 
 
     # Wait to see results
-    time.sleep(10)
+    time.sleep(3)
 
 
     # Get the full page source
     current_url= driver.current_url
-    chat_response = driver.find_element(By.CSS_SELECTOR, "p")
+    chat_response = driver.find_element(By.XPATH, f'//div[@class="gap-2"]/div[@class="space-y-2"]/p[contains(text(), "{word}") and not(contains(text(), "Japanese sentence"))]')
 
+    # Break japanese text from english meaning/romaji
+    japanese_text = ''.join(regex.findall(r'[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}ー。、！？]', chat_response.text))
 
-    # Print part of the HTML
-    print("Current URL:", current_url)
-    print("Chat says:", chat_response.text)
-
+    print(japanese_text)
 
     # Close the browser
     driver.quit()
