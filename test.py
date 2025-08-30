574425bc6e85fb028261e3e1d90d35fb825a6188
Alex-mkn36 <alex.m.k.n36@gmail.com>
2025-08-30 14:39:16 +1000

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import regex
-
-text = "(Poster wo haru kara, heya ga kirei ni narimasu.) Because I put up a poster, the room becomes clean.ポスターを貼るから、部屋がきれいになります。"
-
-# Extract only Japanese characters (Hiragana, Katakana, Kanji)
-japanese_text = ''.join(regex.findall(r'[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}ー。、！？]', text))
-
-print(japanese_text)
